769902e2e0608205c71b68bfcd6e9797ac7c7b55
bm1017553 <brmackeown1@buffs.wtamu.edu>
2022-04-24 18:54:08 -0500

    test update

---
+++ tests/IAttendeeHelperTests/IAttendeeHelperTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using Xunit.Abstractions;
+using domain;
+using domain.AttendeeAggregate;
+using webapp;
+using Microsoft.EntityFrameworkCore;
+
+namespace tests.IAttendeeHelperTests;
+
+public class IAttendeeHelperTests 
+{
+    
+}
