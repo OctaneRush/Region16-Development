13b97ad55a1a91a84c5cd40511d6fc2083dc3f5a
OctaneRush <logantjones77@gmail.com>
2022-04-28 11:31:55 -0500

    Form now loads current user info

--- webapp/Pages/Forms/UpdatingSpeakerForm.cshtml.cs
+++ webapp/Pages/Forms/UpdatingSpeakerForm.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Linq;
 using domain.SpeakerAggregate;
+using domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -11,13 +12,13 @@ namespace webapp.Pages.Forms;
     public class UpdateSpeakerFormModel : PageModel
     {
         private readonly webapp.ApplicationDbContext _context;
-        private readonly IUserEmailStore<IdentityUser>? _emailStore;
-        private readonly UserManager<IdentityUser>? _userManager;
-        private readonly IUserStore<IdentityUser>? _userStore;
+        private IUnitOfWork _unitOfWork;
 
-    public UpdateSpeakerFormModel(webapp.ApplicationDbContext context)
+
+    public UpdateSpeakerFormModel(webapp.ApplicationDbContext context, IUnitOfWork unitOfWork)
     {
         _context = context;
+        _unitOfWork = unitOfWork;
     }
 
         [BindProperty]
@@ -25,26 +26,24 @@ namespace webapp.Pages.Forms;
 
         public async Task<IActionResult> OnGetAsync()
         {
-
-            //var currentUser = await _userStore.FindByNameAsync(User.Identity.Name.ToUpper(), CancellationToken.None);
-            var currentUser = _context.Users.Where(u => u.UserName == _userManager.GetUserName(User));
-            var currentEmail = currentUser.Select(e => e.Email);
-
-
-            // if (email == null)
-            // {
-            //     return NotFound();
-            // }
-    
-            Speaker = _context.Speakers.Where(p => p.EmailAddress.Equals(currentEmail)).SingleOrDefault();
-
-            if (Speaker == null)
+            var currentUser = _context.Speakers.Where(u => u.EmailAddress == HttpContext.User.Identity.Name).FirstOrDefault();
+            
+            if (currentUser != null)
             {
-                return NotFound();
+                Speaker ??= new Speaker
+                {
+                    FirstName = currentUser.FirstName,
+                    LastName = currentUser.LastName,
+                    MailAddress = currentUser.MailAddress,
+                    PrimaryPhoneNumber = currentUser.PrimaryPhoneNumber,
+                    EmailAddress = currentUser.EmailAddress,
+                    JobTitle = currentUser.JobTitle
+                };
+                
+                return Page();
             }
-            else
 
-            return Page();
+            return RedirectToPage("/Index");
         }
 
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -57,28 +56,17 @@ namespace webapp.Pages.Forms;
                 return Page();
             }
 
-            _context.Attach(Speaker).State = EntityState.Modified;
+            var newSpeaker = _unitOfWork.Helper.ValidateSpeaker(Speaker);
 
-            try
+            if (newSpeaker == true)
             {
-                await _context.SaveChangesAsync();
+                await _unitOfWork.Speakers.AddSpeaker(Speaker);           
+                _unitOfWork.Complete();
+                return RedirectToPage("/Index");
             }
-            catch (DbUpdateConcurrencyException)
+            else
             {
-                if (!SpeakerExists(Speaker.EmailAddress))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return Page();
             }
-
-            return RedirectToPage("./Index");
-        }
-        private bool SpeakerExists(string email)
-        {
-            return _context.Speakers.Any(s => s.EmailAddress == email);
         }
     }
